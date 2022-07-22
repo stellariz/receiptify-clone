1da61f160f66db3925a22361a18fa8bd183582cb
stellaris <ruslanchik.popov@gmail.com>
2022-07-22 14:14:30 +0700

    Init commit

---
+++ backend/spotify-app/src/main/java/ru/stellariz/spotifyapp/SpotifyAppApplication.java
@@ -0,0 +1,13 @@
+package ru.stellariz.spotifyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyAppApplication.class, args);
+    }
+
+}
---
+++ backend/spotify-app/src/test/java/ru/stellariz/spotifyapp/SpotifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stellariz.spotifyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
